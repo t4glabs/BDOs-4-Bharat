64e9a0c0ced26abea81ce111a04aeeffcea1600a
Jinso Raj <48287817+JinsoRaj@users.noreply.github.com>
2023-07-27 22:43:37 +0530

    bdo and social worker basic doctype

---
+++ bdo_app/bdos_4_bharath/doctype/block_official/test_block_official.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, T4GLabs and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBlockOfficial(FrappeTestCase):
+	pass
